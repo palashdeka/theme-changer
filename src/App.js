604492b3653fd84dc5d26dbbc7ded29d759033ad
Palash Deka <palashjyoti12@gmail.com>
2024-02-28 22:05:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React , {useState} from "react";
 import './App.css';
 
 function App() {
+  const[color,setColor]=useState("light");
+
+  const ToggleTheme = () => {
+    setColor(prevColor => (prevColor==='light' ? 'dark' : 'light'))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className={`container ${color}`} >
+      <h1>Theme Changer</h1>
+        <button className="button1" onClick={ToggleTheme}>Theme change</button>
     </div>
+    </>
   );
 }
 
